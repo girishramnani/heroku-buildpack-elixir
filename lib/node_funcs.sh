34e0ae71f3b4febf090ea709e0e21c1cc2518d5a
girish ramnani <girishramnani95@gmail.com>
2018-11-22 14:28:32 +0530

    Update node_funcs.sh

--- lib/node_funcs.sh
+++ lib/node_funcs.sh
@@ -1,6 +1,6 @@
-
+export phantom_path="$(platform_tools_path)/phantomjs"
 wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O ${cache_path}/phantom.tar.bz2
-mkdir -p "$(platform_tools_path)/phantomjs"
-cd $(phantomjs_path)
+mkdir -p $phantom_path
+cd $phantom_path
 tar xvjf ${cache_path}/phantom.tar.bz2 
 ls
