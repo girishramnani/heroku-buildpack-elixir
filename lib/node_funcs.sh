0f2834c8db3486291208fb754c2a0ac3697af6da
girish ramnani <girishramnani95@gmail.com>
2018-11-22 19:33:06 +0530

    added nodejs

--- lib/node_funcs.sh
+++ lib/node_funcs.sh
@@ -7,3 +7,12 @@ mv phantomjs-2.1.1-linux-x86_64 phantomjs
 PATH=/app/.platform_tools/phantomjs/phantomjs/bin:${PATH}
 phantomjs --version
 echo "----------------- installed phantomjs ----------------------"
+
+wget https://nodejs.org/dist/v10.13.0/node-v10.13.0-linux-x64.tar.xz -O ${cache_path}/node.tar.xz
+mkdir -p /app/.platform_tools/node
+cd /app/.platform_tools/node
+tar -xf ${cache_path}/node.tar.xz
+mv node-v10.13.0-linux-x64 node
+PATH=/app/.platform_tools/node/node/bin:${PATH}
+npm install -g yarn
+echo "----------------- installed node -----------------------------"
