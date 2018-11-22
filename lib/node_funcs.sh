8e380ccc07fd6c8176527d73ba434814bed216e1
girish ramnani <girishramnani95@gmail.com>
2018-11-22 14:24:38 +0530

    Update node_funcs.sh

--- lib/node_funcs.sh
+++ lib/node_funcs.sh
@@ -1,4 +1,6 @@
 
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar xvjf phantomjs-2.1.1-linux-x86_64.tar.bz2 -C /usr/local/share/
-ln -s /usr/local/share/phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/local/bin/
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O ${cache_path}/phantom.tar.bz2
+mkdir -p $(phantomjs_path)
+cd $(phantomjs_path)
+tar xvjf ${cache_path}/phantom.tar.bz2 
+ls
